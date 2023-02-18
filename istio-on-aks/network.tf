99e93f8a0efbd9828e26358d188da1d32350a86f
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2023-02-17 16:23:59 -0800

    Update network.tf

--- istio-on-aks/network.tf
+++ istio-on-aks/network.tf
@@ -1,10 +1,10 @@
 module "network" {
   source              = "Azure/network/azurerm"
-  vnet_name           = azurerm_resource_group.this.name
-  resource_group_name = azurerm_resource_group.this.name
+  vnet_name           = azurerm_resource_group.aks.name
+  resource_group_name = azurerm_resource_group.aks.name
   address_space       = "10.52.0.0/16"
   subnet_prefixes     = ["10.52.0.0/16"]
   subnet_names        = ["system"]
-  depends_on          = [azurerm_resource_group.this]
+  depends_on          = [azurerm_resource_group.aks]
   use_for_each        = true
 }
